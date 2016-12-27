3a1802b3ab08714f0cbf03ef586535738bbd9836
leconglongvu <leconglongvu@gmail.com>
2016-12-27 13:58:23 +0700

    Clear debug

--- app/src/main/java/com/xtel/vparking/vip/callback/ResponseHandle.java
+++ app/src/main/java/com/xtel/vparking/vip/callback/ResponseHandle.java
@@ -22,33 +22,26 @@ public abstract class ResponseHandle<T extends RESP_Basic> {
     }
 
     public void onSuccess(String result) {
-        Log.e("ResponseHandle", "Result " + result);
         try {
             boolean isJson;
             isJson = !(result == null || result.isEmpty());
 
-            Log.e("ResponseHandle", "Is Json: " + isJson);
             if (!isJson) {
-                Log.e("ResponseHandle", "Success null");
                 onSuccess((T) new RESP_Parking_Info());
             } else {
                 T t = JsonHelper.getObjectNoException(result, clazz);
                 if (t.getError() != null) {
                     onError(t.getError());
-                    Log.e("ResponseHandle", "Error");
                 } else {
                     onSuccess(t);
-                    Log.e("ResponseHandle", "Success");
                 }
             }
         } catch (Exception e) {
-            Log.e("ResponseHandle", "Error parse: " + e.toString());
             onError(new Error(-1, "ERROR_PARSER_RESPONSE", e.getMessage()));
         }
     }
 
     public void onError(IOException error) {
-        Log.e("ResponseHandle", "Error: " + error.getMessage());
         onError(new Error(-1, "ERROR_PARSER_RESPONSE", error.getMessage()));
     }
 
